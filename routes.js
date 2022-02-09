6fd7b3ac679faea912e5268c7d5270818e6c4597
Celio Caus Junior <celiocausjunior@gmail.com>
2022-02-09 20:54:55 -0300

    Assignament one completed

--- routes.js
+++ routes.js
@@ -4,16 +4,33 @@ const requestHandler = (req, res) => {
   const method = req.method
 
   if (url === '/') {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Welcome!')
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
   }
-  if (url === '/users') {
+  else if (url === '/users') {
     res.write('<html>')
     res.write('<head><title>Users List</title><head>')
     res.write('<body><el><li>User One</li><li>User Two</li></el></body>')
     res.write('</html>')
     return res.end()
+  }else if (url === '/create-user') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody.split('=')[1]); 
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
   }
 }
 
